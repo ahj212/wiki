5acf4b8d004aa274e13753a78337966cdd74b860
Hwan Ho Jeoung <ahj212@gmail.com>
2015-07-21 17:40:08 -0400

    started user session

--- server.js
+++ server.js
@@ -1,43 +1,3 @@
-var express = require('express');
-var app = express();
-var fs = require('fs');
-var root = __dirname;
-var bodyParser = require('body-parser');
-var exphbs = require('express-handlebars');
-var methodOverride = require('method-override');
-var logger = require('morgan');
-var path = require('path');
-var db = require('./db.js');
-
-app.engine('handlebars', exphbs({defaultLayout: 'main', extname: 'handlebars'}));
-app.set('views', path.join(root, 'views'));
-app.set('view engine', 'handlebars');
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use(logger('dev'));
-
-app.use(methodOverride(function (req, res) {
-	if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-		var method = req.body._method;
-		delete req.body._method;
-		return method;
-	}
-}));
-
-fs.readdirSync('./controllers').forEach(function (file) {
-	if(file.substr(-3) == '.js') {
-		route = require('./controllers/' + file);
-		console.log('this is the route', route);
-		route.controller(app);
-	}
-});
-
-//ROOT ROUTE
-app.get('/', function (req, res) {
-	res.render('home');
-});
-
 //loading npm
 var express = require('express');
 var logger = require('morgan');
@@ -46,6 +6,9 @@ var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var pg = require('pg');
+var bcrypt = require('bcrypt');
+var session = require('express-session');
+
 
 var app = express();
 
@@ -56,6 +19,15 @@ app.set('view engine', 'handlebars');
 
 //setting up body-parser
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//setting up sessions
+app.use(session({
+  secret: 'allthethings',
+  saveUninitialized: false,
+  resave: false
+}));
+
 
 //loading static files
 app.use(express.static('public'));
